cd83a40501d3217c27c7fd09c4c026a8e39d2671
Thomas Turner <thomas_turner@talktalk.net>
2025-03-13 17:32:51 +0000

    Add <partition>/addon.d support to fsgen
    This is needed for a script in vendor/lineage
    Test: m on exynos device with
    PRODUCT_COPY_FILES += \
        vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
    
    Change-Id: I75f20daa390fdbea2e808b2bbeab37b3bc27a536
    Signed-off-by: ryzenforce990 <superxorn@horizondroid.com>

--- etc/prebuilt_etc.go
+++ etc/prebuilt_etc.go
@@ -65,6 +65,7 @@ func RegisterPrebuiltEtcBuildComponents(ctx android.RegistrationContext) {
 	ctx.RegisterModuleType("prebuilt_firmware", PrebuiltFirmwareFactory)
 	ctx.RegisterModuleType("prebuilt_gpu", PrebuiltGPUFactory)
 	ctx.RegisterModuleType("prebuilt_install", PrebuiltInstallFactory)
+	ctx.RegisterModuleType("prebuilt_addon.d", PrebuiltAddonDFactory)
 	ctx.RegisterModuleType("prebuilt_dsp", PrebuiltDSPFactory)
 	ctx.RegisterModuleType("prebuilt_rfsa", PrebuiltRFSAFactory)
 	ctx.RegisterModuleType("prebuilt_renderscript_bitcode", PrebuiltRenderScriptBitcodeFactory)
@@ -803,6 +804,15 @@ func PrebuiltGPUFactory() android.Module {
 func PrebuiltInstallFactory() android.Module {
 	module := &PrebuiltEtc{}
 	InitPrebuiltEtcModule(module, "install")
+	// This module is device-only
+	android.InitAndroidArchModule(module, android.DeviceSupported, android.MultilibFirst)
+	android.InitDefaultableModule(module)
+	return module
+}
+
+func PrebuiltAddonDFactory() android.Module {
+	module := &PrebuiltEtc{}
+	InitPrebuiltEtcModule(module, "addon.d")
 	// This module is device-only
 	android.InitAndroidArchModule(module, android.DeviceSupported, android.MultilibFirst)
 	android.InitDefaultableModule(module)
